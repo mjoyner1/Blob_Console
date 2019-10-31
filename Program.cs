53a4cc90d28c14441384df9817cb2df1d47e3894
Michael Joyner <mjoyner1@my.wctc.edu>
2019-10-30 20:57:19 -0500

    EF-Blogs-Assignment
    Changes to code for 10/30 EF-Bogs-Assign

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace BlogsConsole
                 Console.WriteLine("Press 1 to display all Blogs: ");
                 Console.WriteLine("Press 2 to add a Blog: ");
                 Console.WriteLine("Press 3 to Create a post: ");
-                Console.WriteLine("Press 4 to Exit Program:");
+                Console.WriteLine("Press 4 to display post: ");
+                Console.WriteLine("Enter q to quit: ");
                 int input = 0;
                 string fix = Console.ReadLine();
                 if (!int.TryParse(fix,out input))
@@ -36,8 +37,10 @@ namespace BlogsConsole
                         var db = new BloggingContext();
                         // Display all Blogs from the database
                         var query = db.Blogs.OrderBy(b => b.Name);
+                        int num = query.Count();
 
-                        Console.WriteLine("All blogs in the database:");
+
+                        Console.WriteLine($"{num} blogs in the database:");
                         foreach (var item in query)
                         {
                             Console.WriteLine(item.Name);
@@ -48,13 +51,22 @@ namespace BlogsConsole
                         // Create and save a new Blog
                         Console.Write("Enter a name for a new Blog: ");
                         var name = Console.ReadLine();
+                        //blank blog entry error
+                        if (name.Length == 0)
+                        {
+                            logger.Error("Blog name cannot be null");
+                        }
+                        else
+                        {
+                            var blog = new Blog { Name = name };
 
-                        var blog = new Blog { Name = name };
-
-                        var db = new BloggingContext();
-                        db.AddBlog(blog);
-                        logger.Info("Blog added - {name}", name);
+                            var db = new BloggingContext();
+                            db.AddBlog(blog);
+                            logger.Info("Blog added - {name}", name);
+                            logger.Error("Blog name cannot be null");
+                        }
                     }
+
                     else if (input == 3)
                     {
                         //Create and save new Post
@@ -69,23 +81,60 @@ namespace BlogsConsole
                         }
                         Console.WriteLine("Select the blog you would like to post to: ");
                         var blog = Console.ReadLine();
-                        var query1 = db.Blogs.Where(b => b.Name.ToLower() == blog.ToLower()).First();
-                        Console.WriteLine("Enter the title of your Post: ");
-                        var title = Console.ReadLine();
+                        var query1 = db.Blogs.Where(b => b.Name.ToLower() == blog.ToLower()).FirstOrDefault();
+                        //Invalid Blog Name or no blog name entered error
+                        if (blog.Length == 0)
+                        {
+                            logger.Error("Blog Name is not found");
+                        }
+                        else if (query1 == null)
+                        {
+                            logger.Error("Blog Name is not found");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter the title of your Post: ");
+                            var title = Console.ReadLine();
+                            //Error when no title entered
+                            if (title.Length == 0)
+                            {
+                                logger.Error("Post title cannot be null");
 
-                        Console.Write("Enter your Post Content: ");
-                        var content = Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.Write("Enter your Post Content: ");
+                                var content = Console.ReadLine();
+                                if (content.Length == 0)
+                                {
+                                    logger.Error("Post content cannot be null");
 
-                        var post = new Post { Title = title, Content = content, BlogId = query1.BlogId };
 
-                        db.AddPost(post);
-                        logger.Info("Post added - {content}", content);
+                                }
+                                else
+                                {
+                                    var post = new Post { Title = title, Content = content, BlogId = query1.BlogId };
+
+
+
+                                    db.AddPost(post);
+                                    logger.Info("Post added - {content}", content);
+                                }
+                            }
+                        }
                     }
                     else if (input == 4)
                     {
+                        //display Post
+                        var db = new BloggingContext();
+
+                        var query = db.Posts.OrderBy(p => p.Content);
+
+                    }
+                    else if (input == 'q')
+                    {
                         cont = false;
                     }
-
 
                 }
                 catch (Exception ex)
